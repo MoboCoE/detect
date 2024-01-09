ea410575487df362cd2516ec3a61f8802137e6e3
nnbaitei <nnbaitei@gmail.com>
2024-01-09 12:58:06 +0700

    use hsv for detect yellow in image

--- detect_raspi.py
+++ detect_raspi.py
@@ -4,9 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import zscore
 import time
+from util import get_limits
 
 try:
     cap = cv2.VideoCapture("/dev/video0")
+
+    yellow = [0, 255, 255]
 
     if not cap.isOpened():
         print("Error: Could not open camera.")
@@ -27,30 +30,31 @@ try:
             
             resize_img = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA) 
 
-            # grayscale
-            gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            hsvImage = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV)
+            lowerLimit, upperLimit = get_limits(color=yellow)
+
+            mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+            # # grayscale
+            # gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
-            # threshold image
-            (thresh, thresh_img) = cv2.threshold(gray_img, 168, 255, cv2.THRESH_BINARY)
+            # # threshold image
+            # (thresh, thresh_img) = cv2.threshold(gray_img, 168, 255, cv2.THRESH_BINARY)
 
             # change image to array
-            array_img = asarray(thresh_img)
+            array_img = asarray(mask)
 
             col = len(array_img[0, :])
             row = len(array_img[:, 0])
             center_x = col/2
             center_y = row/2
-            # print(col, row)
-            # print(center_x, center_y)
 
-            # print(width, height)
-            # print(col, row) 275, 206
             middle_line = []
             x_axis = []
             y_axis = []
 
-            for i in range(row):
+            for i in range(0, row-1, 6):
                 find_line = []
                 index = 0
                 for j in array_img[i, :]:
@@ -60,12 +64,11 @@ try:
                         af_array = array_img[i, index+1]
                         # print(index)
                         
-                        if j == 0 and bf_array == 0 and af_array == 0:
+                        if j == 255 and bf_array == 255 and af_array == 255:
                             find_line.append(index)
                             # print(bf_array, j, af_array)
-                    if j == 0 and index == 0:
+                    if j == 255 and index == 0:
                         find_line.append(index)
-                
                         
                     index += 1
                 if len(find_line) != 0:
@@ -85,35 +88,9 @@ try:
                 x = np.array(x_axis, dtype=float)
                 y = np.array(y_axis, dtype=float)
 
-                # delete outlier
-                z_scores = zscore(y)
+                a, b = np.polyfit(x, y, 1)
+                print(f"true,{a},{b}")
 
-                threshold = 3
-
-                filtered_indices = np.where(np.abs(z_scores) < threshold)
-                filtered_x = x[filtered_indices]
-                filtered_y = y[filtered_indices]
-
-                # plt.scatter(x, y, label='Original Data')
-                # plt.scatter(filtered_x, filtered_y, label='Filtered Data', color='red')
-                # plt.legend()
-                # plt.show()
-
-                # find a, b
-                
-                if len(filtered_x) == 0 or len(filtered_y) == 0:
-                    a, b = np.polyfit(x, y, 1)
-                    print(f"true,{a},{b}")
-                else:
-                    a, b = np.polyfit(filtered_x, filtered_y, 1)
-
-                # plt.scatter(filtered_x, filtered_y)
-
-                    print(f"true,{a},{b}")
-
-                # plt.plot(filtered_x, a*(filtered_x) + b, 'r')
-                # plt.grid()
-                # plt.show()
             else:
                 print(f"false,NaN,NaN")
 
