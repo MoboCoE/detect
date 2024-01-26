ba1b70a47f6cf00a2fa5bf6974968db3df44e52c
nnbaitei <nnbaitei@gmail.com>
2024-01-26 14:15:40 +0700

    code for locomotion

--- linux/v_motor.py
+++ linux/v_motor.py
@@ -1,34 +1,70 @@
-
+import math
+from send_serial import get_serial, send_data
+import time
+global state
 def velocity(status, x, y):
-
-    setpoint_slope = 0.02
-    slope = x
-    base_speed = 150
+    
+    slope = float(x)
+    intersect = float(y)
+    base_speed = 120
     max_speed = 200
-    k_slope = 100
+    k_slope = 50
+    maxline = 60
+    minline = 30
+    a = 30
 
     if status == "true":
-        if -setpoint_slope <= slope <= setpoint_slope:
-            right_speed = base_speed
-            left_speed = base_speed
+        state = 0
 
-        if slope > setpoint_slope:
-            right_speed = base_speed + k_slope*slope
-            left_speed = base_speed - k_slope*slope
+        if (slope < 0 and intersect < 0) or (slope > 0 and intersect > 0):
+            if -1 < slope < 1 and minline <= intersect <= maxline:
+                right_speed = base_speed
+                left_speed = base_speed
+                print("ตรง")
+                print(left_speed, right_speed)
+                
+                
+            elif intersect > maxline or intersect < minline:
+                if intersect > maxline:
+                    
+                    send_data(intersect*10, 0)
+                    print("เลี้ยวขวาตรง", intersect)
+                    right_speed = 0
+                    left_speed = a
+                    
+                if intersect < minline:
+                    if intersect<0:
+                        intersect = -intersect-minline
+                    send_data(0, intersect*20)
+                    print("เลี้ยวซ้ายตรง", intersect)
+                    right_speed = a
+                    left_speed = 0
 
+        elif slope > 0 and intersect < 0:
+            right_speed = 80 + k_slope*slope + (-intersect)/60
+            left_speed = 80 - k_slope*slope - (-intersect)/20
+            print("เลี้ยวซ้าย")
+            print(left_speed, right_speed)
 
-        if slope < -setpoint_slope:
+        elif slope < 0 and intersect > 0:
             slope = -slope
-            right_speed = base_speed - k_slope*slope
-            left_speed = base_speed + k_slope*slope
-    
-    else:
-        right_speed = 0
-        left_speed = 0
+            right_speed = 80 - k_slope*slope - intersect/60
+            left_speed = 80 + k_slope*slope + intersect/20
+            print("เลี้ยวขวา")
+            print(left_speed, right_speed)
 
+    else:
+        if x != 0 and y != 0:
+            right_speed = x
+            left_speed = y
+            print(left_speed, right_speed)
+        else:
+            right_speed = 0
+            left_speed = 0
+            print(left_speed, right_speed)
+            
     # Limit the motor speeds to the maximum value
     left_speed = max(-max_speed, min(max_speed, left_speed))
     right_speed = max(-max_speed, min(max_speed, right_speed))
-
 
     return right_speed, left_speed
